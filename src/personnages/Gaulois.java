caa8e0cbdc575a1975f3a151ba0ccec134084414
Lauriana Be manantsoa <bml5131a@ensfsi.onmicrosoft.com>
2024-09-19 11:37:45 +0200

    classe gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	public int getForce() {
+		return force;
+	}
+	public void setForce(int force) {
+		this.force = force;
+	}
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+	public void setEffetPotion(int effetPotion) {
+		this.effetPotion = effetPotion;
+	}
+	
+
+}
